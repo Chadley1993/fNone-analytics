fadc38ebf4c83378e2f0334a4ff6d9942703ad60
Chadley Maggott <cbmaggott@gmail.com>
2025-02-09 12:25:56 +0200

    sui button changes

--- backyard-analytics/src/app/control-plane/control-plane.component.ts
+++ backyard-analytics/src/app/control-plane/control-plane.component.ts
@@ -1,30 +1,30 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule, MatSlideToggleChange } from '@angular/material/slide-toggle';
 import { MyService } from '../my-service.service';
 
 @Component({
   selector: 'app-control-plane',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, MatButtonModule],
   templateUrl: './control-plane.component.html',
   styleUrl: './control-plane.component.scss'
 })
 export class ControlPlaneComponent {
   // private theService: MyService;
+  connectionBtnColor: string = '#424242';
   constructor(private myService: MyService) {
     // this.theService = myService;
   }
+  toggleState: boolean = false;
 
-  onToggle(event: MatSlideToggleChange) {
-    console.log('Toggle state:', event.checked);
+  onToggleConnect() {
+    console.log(this.connectionBtnColor)
+    this.connectionBtnColor = this.connectionBtnColor == '#424242' ? 'red' : '#424242'
+  }
 
-    if (event.checked) {
-      this.startStreaming();
-      this.myService.startDataStream();
-    } else {
-      this.endStreaming();
-      this.myService.stopDataStream();
-    }
+  onToggleRecord() {
+    console.log("click")
   }
 
   startStreaming() {
